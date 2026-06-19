39439752d8949e226ef0b1b070f95ca6b9544548
Maks <Maxym.Abakumov@gmail.com>
2026-06-19 18:48:10 +0300

    Update donation progress

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ animatedElements.forEach((el) => {
 
     82500
 */
-const collectedAmount = 0;
+const collectedAmount = 102300.5;
 const targetAmount = 200000;
 const progressPercent = Math.min(Math.round((collectedAmount / targetAmount) * 100), 100);
 const fernProgress = document.querySelector(".fern-progress");
